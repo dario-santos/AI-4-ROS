67153f7523c146757c0a3aa433480bce255f555b
dario-santos <dariovfsantos@gmail.com>
2019-11-25 17:47:06 +0000

    Adicionado o projeto em si ao repositório
    Atualizado o readme com instruções de instalação.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IA-4-ROS
 IA for ROS é um trabalho desenvolvido para a Unidade Curricular Inteligência Artificial na Universidade da Beira Interior.
+
+## How to install
+
+Substitui o diretório /home/viki/catkin_ws/src/ia/src pelo presente no repositório.
+
---
+++ src/questions_keyboard.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf8
+import rospy
+from std_msgs.msg import Int32,String
+import sys
+import tty
+
+# ---------------------------------------------------------------
+def questions():
+	# node name
+	rospy.init_node('questions_keyboard')
+	pub=rospy.Publisher('questions_keyboard',String,queue_size=10)
+	r = rospy.Rate(10)
+	
+	# show questions
+	print ' ************************'
+	print '    QUESTIONS '
+	print ' ************************'
+	print '  1-How many rooms are not occupied?'
+	print '  2-How many suites did you find until now?'
+	print '  3-Is it more probable to find people in the corridors or inside the rooms?'
+	print '  4-If you want to find a computer, to which type of room do you go to?'
+	print '  5-What is the number of the closest single room?'
+	print '  6-How can you go from the current room to the elevator?'
+	print '  7-How many books do you estimate to find in the next 2 minutes?'
+	print '  8-What is the probability of finding a table in a room without books but that has at least one chair?'
+ 
+	tty.setcbreak(sys.stdin)
+
+	while not rospy.is_shutdown():
+		# read from keyboard
+		k=sys.stdin.read(1)
+		if int(k) < 1 or int(k) >8:
+			continue
+		pub.publish(k)
+		#print 'Asked question: ' , k
+		
+# ---------------------------------------------------------------
+if __name__ == '__main__':
+	questions()
